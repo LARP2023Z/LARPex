fab74b0c3f0755557431868084c99d98e3f3f6f3
Ignacy Ruszpel <8687205+iruszpel@users.noreply.github.com>
2023-11-06 21:52:05 +0100

    feat-fe: adjust components to class diagram

--- frontend/src/payments/PaymentsPage.tsx
+++ frontend/src/payments/PaymentsPage.tsx
@@ -1,38 +1,19 @@
 import { FC } from 'react';
-import {
-  Button,
-  GroupBox,
-  Select,
-  TextInput,
-  Window,
-  WindowContent,
-  WindowHeader,
-} from 'react95';
+import { Window, WindowContent, WindowHeader } from 'react95';
 import Draggable from 'react-draggable';
 import { Space } from 'src/common/Space';
+import { PaymentMethodPresenter } from './PaymentMethodPresenter';
+import { PayPresenter } from './PayPresenter';
 
 export const PaymentsPage: FC = () => {
-  const options = [
-    { value: 'Przelew', label: 'Przelew' },
-    { value: 'Karta', label: 'Karta' },
-    { value: 'PayPal', label: 'PayPal' },
-  ];
-
   return (
     <Draggable>
       <Window>
         <WindowHeader>Płatność</WindowHeader>
         <WindowContent>
           <Space gap={8}>
-            <GroupBox label="Kwota do zapłaty">
-              <TextInput defaultValue="123.45 PLN" disabled />
-            </GroupBox>
-
-            <GroupBox label="Metoda płatności">
-              <Select options={options} />
-            </GroupBox>
-
-            <Button fullWidth>Płatność</Button>
+            <PaymentMethodPresenter />
+            <PayPresenter />
           </Space>
         </WindowContent>
       </Window>
