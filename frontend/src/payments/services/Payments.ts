51a9589adb6e2677d41b6bedd096dd3a1bd8516f
sobczal2 <81999946+sobczal2@users.noreply.github.com>
2023-11-11 18:22:32 +0100

    feat-fe: Add model-viewmodel-view-controller-presenter architecture part1

---
+++ frontend/src/payments/services/Payments.ts
@@ -0,0 +1,6 @@
+export class PaymentProxyMock implements IPayments {
+  payForEvent(eventId: string, userId: string): Promise<void> {
+    return Math.random() > 0.5 ? Promise.resolve() : Promise.reject();
+  }
+
+}
