98b2b0281d4b6f57437ca408a32e21630639bfd2
Ignacy Ruszpel <8687205+iruszpel@users.noreply.github.com>
2023-11-12 22:03:28 +0100

    wip

--- frontend/src/payments/services/IPayments.ts
+++ frontend/src/payments/services/IPayments.ts
@@ -1,3 +1,6 @@
+import { PaymentMethod } from '../types/PaymentMethod';
+
 export interface IPayments {
   payForEvent(eventId: string, userId: string): Promise<void>;
+  getPaymentMethods(): Promise<PaymentMethod[]>;
 }
