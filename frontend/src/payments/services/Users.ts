51754a5a82461f0534765ef0bdbbc977947b7191
Ignacy Ruszpel <8687205+iruszpel@users.noreply.github.com>
2023-11-14 13:22:49 +0100

    feat: add CI and fix formatting

--- frontend/src/payments/services/Users.ts
+++ frontend/src/payments/services/Users.ts
@@ -1,9 +1,11 @@
-import dayjs from "dayjs";
-import { UserDto } from "../dtos/userDtos";
-import { IUsers } from "./IUsers";
+import dayjs from 'dayjs';
+import { UserDto } from '../dtos/userDtos';
+import { IUsers } from './IUsers';
 
 export class UserProxyMock implements IUsers {
   getUser(userId: string): Promise<UserDto> {
-    return Promise.resolve(new UserDto(userId, "alias", "name", "surname", dayjs()));
+    return Promise.resolve(
+      new UserDto(userId, 'alias', 'name', 'surname', dayjs())
+    );
   }
 }
