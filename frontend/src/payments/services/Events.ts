15b96f8547ef34ddf57fccc051ff3c685afd48da
Ignacy Ruszpel <8687205+iruszpel@users.noreply.github.com>
2023-11-14 13:39:07 +0100

    fix linting

--- frontend/src/payments/services/Events.ts
+++ frontend/src/payments/services/Events.ts
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 import { IEvents } from './IEvents';
 
 export class EventProxyMock implements IEvents {
-  getEventDetails(eventId: string) {
+  getEventDetails(_eventId: string) {
     // Mock
     return Promise.resolve({
       uuid: crypto.randomUUID(),
