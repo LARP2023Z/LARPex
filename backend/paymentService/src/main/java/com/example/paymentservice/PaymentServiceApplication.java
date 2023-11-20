31c819271874180db6471abf38f4301fb7a21244
krzys <krzysiekrudnik7@gmail.com>
2023-11-20 09:19:50 +0100

    Database works

--- backend/paymentService/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ backend/paymentService/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -2,12 +2,19 @@ package com.example.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
-
+    @GetMapping("/hello")
+    public String hello() {
+        return "xdd";
+    }
 }
