1a16a6f1cfb2dd9815b764e3f0ff1d7e84773ebc
kajamyk <73692436+kajamyk@users.noreply.github.com>
2023-11-20 21:19:11 +0100

    Update Placeholder.tsx

--- frontend/src/common/Placeholder.tsx
+++ frontend/src/common/Placeholder.tsx
@@ -1,38 +1,42 @@
 import {
-    Button,
-    MenuList,
-    MenuListItem,
-    Separator,
-    Window,
-    WindowContent,
-    WindowHeader,
+  MenuList,
+  MenuListItem,
+  Separator,
+  Window,
+  WindowContent,
+  WindowHeader,
 } from 'react95';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Draggable from 'react-draggable';
 
 type PlaceholderProps = {
-    title: string;
+  title: string;
 };
-const Placeholder = ({title}: PlaceholderProps) => {
-    return (
-        <Draggable>
-            <Window>
-                <WindowHeader>Placeholder dla strony {title}</WindowHeader>
-                <WindowContent>
-                    <MenuList inline>
-                        {['/', '/login', '/events', '/payments'].map((path, i) => (
-                            <>
-                                {i !== 0 && <Separator orientation="vertical" size="43px"/>}
-                                <MenuListItem>
-                                    <Link to={path}>Idź do {path}</Link>
-                                </MenuListItem>
-                            </>
-                        ))}
-                    </MenuList>
-                </WindowContent>
-            </Window>
-        </Draggable>
-    );
+const Placeholder = ({ title }: PlaceholderProps) => {
+  const navigationItems =
+    localStorage.getItem('userName') !== null
+      ? ['/', '/events', '/payments']
+      : ['/', '/login'];
+
+  return (
+    <Draggable>
+      <Window>
+        <WindowHeader>Placeholder dla strony {title}</WindowHeader>
+        <WindowContent>
+          <MenuList inline>
+            {navigationItems.map((path, i) => (
+              <>
+                {i !== 0 && <Separator orientation="vertical" size="43px" />}
+                <MenuListItem>
+                  <Link to={path}>Idź do {path}</Link>
+                </MenuListItem>
+              </>
+            ))}
+          </MenuList>
+        </WindowContent>
+      </Window>
+    </Draggable>
+  );
 };
 
 export default Placeholder;
