18eadb61ec6b1041e75e5581170d93061b592c77
Ignacy Ruszpel <8687205+iruszpel@users.noreply.github.com>
2024-01-08 18:04:27 +0100

    feat: complete payment flow with events

--- frontend/src/payments/services/Payments.ts
+++ frontend/src/payments/services/Payments.ts
@@ -1,17 +1,32 @@
+import { api } from 'src/api/apiClient';
 import { PaymentMethod } from '../types/PaymentMethod';
-import { IPayments } from './IPayments';
+import { IPayments, PaymentMethodDto } from './IPayments';
 
 export class PaymentProxyMock implements IPayments {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  payForEvent(eventId: string, userId: string): Promise<void> {
-    return Math.random() > 0.5 ? Promise.resolve() : Promise.reject();
+  async payForEvent(
+    eventId: string,
+    userId: string,
+    amount: number,
+    method: PaymentMethodDto
+  ): Promise<void> {
+    await api.payments.postConfirm({
+      amount: amount,
+      eventId: eventId,
+      userId: userId,
+      paymentMethod: method,
+    });
+
+    await api.payments.getConfirm({
+      eventId: eventId,
+      userId: userId,
+    });
   }
 
   getPaymentMethods(): Promise<PaymentMethod[]> {
     return Promise.resolve([
       { name: 'Przelew', icon: 'Przelew' },
-      { name: 'Karta', icon: 'Karta' },
-      { name: 'PayPal', icon: 'PayPal' },
+      { name: 'BLIK', icon: 'BLIK' },
     ]);
   }
 }
